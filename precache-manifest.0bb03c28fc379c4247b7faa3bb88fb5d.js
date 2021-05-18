96b2ab660527b6c4ffd4c7e9c622120f037d3c26
wsielyfeng <learnroad@163.com>
2021-05-18 15:22:42 +0800

    deploy

---
+++ precache-manifest.0bb03c28fc379c4247b7faa3bb88fb5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e318315f14113072a61",
+    "url": "/fishKeeping-website/css/app.4a784653.css"
+  },
+  {
+    "revision": "293bfbab79f26307e2ff925cb57c541a",
+    "url": "/fishKeeping-website/img/qrcode.293bfbab.png"
+  },
+  {
+    "revision": "603a7a472eb40df8026e0683e9a1ba6b",
+    "url": "/fishKeeping-website/index.html"
+  },
+  {
+    "revision": "3e318315f14113072a61",
+    "url": "/fishKeeping-website/js/app.c9bc021a.js"
+  },
+  {
+    "revision": "006df4638972420f34cb",
+    "url": "/fishKeeping-website/js/chunk-vendors.ad03bc71.js"
+  },
+  {
+    "revision": "54f7252a42d896aa08a405c95cb65bb2",
+    "url": "/fishKeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fishKeeping-website/robots.txt"
+  }
+]);
